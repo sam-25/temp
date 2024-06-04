65e704fe776ae576406e9952837d408d968f03b1
sam-25 <73120494+sam-25@users.noreply.github.com>
2024-06-04 09:39:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,76 @@
-import React, { useState } from "react";
-import { appTheme } from "../../../theme";
-import { SciChartGroup, SciChartReact } from "scichart-react";
-import classes from "../../../styles/Examples.module.scss";
-import { getChartsInitializationAPI } from "./drawExample";
-import { SciChartSurface } from "scichart";
+import React, { useState, useEffect } from 'react';
+import ReactApexChart from 'react-apexcharts';
 
-export default function MultiPaneStockCharts() {
-    const [chartsInitializationAPI] = useState(getChartsInitializationAPI());
-    const [mainChart, setMainChart] = useState(null);
+const SyncedCharts = () => {
+  const [lineChartOptions, setLineChartOptions] = useState({
+    series: [{
+      name: 'Line Series',
+      data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+    }],
+    colors: ['#00E396'],
+    chart: {
+      id: 'line-1',
+      group: 'social',
+      type: 'line',
+    },
+    yaxis: {
+      labels: {
+        minWidth: 40
+      }
+    }
+  });
 
-    return (
-        <div className={classes.ChartWrapper}>
-            <SciChartGroup onInit={chartsInitializationAPI.configureAfterInit}>
-                <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
-                    {/* The panel hosting the price chart */}
-                    <SciChartReact
-                        initChart={chartsInitializationAPI.drawPriceChart}
-                        style={{ flexBasis: 400, flexGrow: 1, flexShrink: 1 }}
-                        onInit={({ sciChartSurface }) => setMainChart(sciChartSurface)}
-                    />
-                    {/* Panels hosting the Macd and RSI Indicator charts */}
-                    <SciChartReact
-                        initChart={chartsInitializationAPI.drawMacdChart}
-                        style={{ flexBasis: 100, flexGrow: 1, flexShrink: 1 }}
-                    />
-                    <SciChartReact
-                        initChart={chartsInitializationAPI.drawRsiChart}
-                        style={{ flexBasis: 100, flexGrow: 1, flexShrink: 1 }}
-                    />
-                    {/* Panel hosting the overview control */}
-                    {mainChart ? (
-                        <SciChartReact
-                            initChart={chartsInitializationAPI.drawOverview(mainChart)}
-                            style={{ flexBasis: "70px" }}
-                        />
-                    ) : null}
-                </div>
-            </SciChartGroup>
-        </div>
-    );
-}
+  const [areaChartOptions, setAreaChartOptions] = useState({
+    series: [{
+      name: 'Area Series',
+      data: [23, 42, 35, 27, 43, 22, 17, 29, 43]
+    }],
+    colors: ['#008FFB'],
+    chart: {
+      id: 'area-1',
+      group: 'social',
+      type: 'area',
+    },
+    yaxis: {
+      labels: {
+        minWidth: 40
+      }
+    }
+  });
+
+  const globalOptions = {
+    chart: {
+      height: 160,
+    },
+    dataLabels: {
+      enabled: false
+    }
+  };
+
+  useEffect(() => {
+    window.Apex = globalOptions;
+  }, [globalOptions]);
+
+  return (
+    <div id="synced-charts">
+      <div id="chart-line">
+        <ReactApexChart
+          options={lineChartOptions}
+          series={lineChartOptions.series}
+          type="line"
+          height={160}
+        />
+      </div>
+      <div id="chart-area">
+        <ReactApexChart
+          options={areaChartOptions}
+          series={areaChartOptions.series}
+          type="area"
+          height={160}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SyncedCharts;
